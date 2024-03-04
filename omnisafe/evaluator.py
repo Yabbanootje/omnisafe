1ba6f22508706f04a9564f42a252b42fdd3618a5
Yabbanootje <k.a.b.toonen@student.tue.nl>
2024-03-04 16:28:24 +0100

    Use options for render

--- omnisafe/evaluator.py
+++ omnisafe/evaluator.py
@@ -471,7 +471,7 @@ class Evaluator:  # pylint: disable=too-many-instance-attributes
         horizon = 1000
         frames = []
         print("resetting for render")
-        obs, _ = self._env.reset(resetting_for_render = True)
+        obs, _ = self._env.reset(options={"resetting_for_render": True})
         if self._render_mode == 'human':
             self._env.render()
         elif self._render_mode == 'rgb_array':
