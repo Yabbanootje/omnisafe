1153353a65081be747131d4a557869a47d88ca75
Yabbanootje <k.a.b.toonen@student.tue.nl>
2024-04-17 13:00:01 +0200

    Remove print

--- omnisafe/common/logger.py
+++ omnisafe/common/logger.py
@@ -380,7 +380,6 @@ class Logger:  # pylint: disable=too-many-instance-attributes
 
     def close(self) -> None:
         """Close the logger."""
-        print("closing logger...")
         if self._use_wandb:
             wandb.finish()
         if self._maste_proc:
