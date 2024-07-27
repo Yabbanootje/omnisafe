25fb02c18864dcf343472b49afe6b681bf987e85
Yabbanootje <k.a.b.toonen@student.tue.nl>
2024-07-27 10:14:32 +0200

    Add loading of lag_mul to some other algos

--- omnisafe/algorithms/on_policy/first_order/focops.py
+++ omnisafe/algorithms/on_policy/first_order/focops.py
@@ -26,6 +26,7 @@ from omnisafe.algorithms.on_policy.base.policy_gradient import PolicyGradient
 from omnisafe.common.lagrange import Lagrange
 from omnisafe.utils import distributed
 
+from omnisafe.algorithms.on_policy.base.policy_gradient import *
 
 @registry.register
 class FOCOPS(PolicyGradient):
@@ -228,3 +229,107 @@ class FOCOPS(PolicyGradient):
                 'Metrics/LagrangeMultiplier': self._lagrange.lagrangian_multiplier,
             },
         )
+
+    def __load_model_and_env(
+        self,
+        epoch: int,
+        path: str,
+    ) -> None:
+        """Load the model from the save directory.
+
+        Args:
+            epoch (int): The epoch that should be loaded
+            path (str): The path to where the model is saved.
+
+        Raises:
+            FileNotFoundError: If the model is not found.
+        """
+        # load the saved model
+        model_path = os.path.join(path, 'torch_save', f'epoch-{epoch}.pt')
+        try:
+            model_params = torch.load(model_path)
+        except FileNotFoundError as error:
+            raise FileNotFoundError('The model is not found in the save directory.') from error
+        
+        # Redo _init_env with loaded parameters       
+        if re.search(r"From(\d+|T)HMA(\d+|T)", self._env_id) is not None:
+            self._env: OnPolicyAdaptiveCurriculumAdapter = OnPolicyAdaptiveCurriculumAdapter(
+                self._env_id,
+                self._cfgs.train_cfgs.vector_env_nums,
+                self._seed,
+                self._cfgs,
+            )
+        else:
+            self._env: OnPolicyCurriculumAdapter = OnPolicyCurriculumAdapter(
+                self._env_id,
+                self._cfgs.train_cfgs.vector_env_nums,
+                self._seed,
+                self._cfgs,
+            )
+            self._env.rewrap(model_params)
+        assert (self._cfgs.algo_cfgs.steps_per_epoch) % (
+            distributed.world_size() * self._cfgs.train_cfgs.vector_env_nums
+        ) == 0, 'The number of steps per epoch is not divisible by the number of environments.'
+        self._steps_per_epoch: int = (
+            self._cfgs.algo_cfgs.steps_per_epoch
+            // distributed.world_size()
+            // self._cfgs.train_cfgs.vector_env_nums
+        )
+
+        # Redo _init_model with loaded parameters
+        self._actor_critic: ConstraintActorCritic = ConstraintActorCritic(
+            obs_space=self._env.observation_space,
+            act_space=self._env.action_space,
+            model_cfgs=self._cfgs.model_cfgs,
+            epochs=self._cfgs.train_cfgs.epochs,
+        ).to(self._device)
+        self._actor_critic.load_state_dict(model_params['actor_critic'])
+        
+        if distributed.world_size() > 1:
+            distributed.sync_params(self._actor_critic)
+
+        if self._cfgs.model_cfgs.exploration_noise_anneal:
+            self._actor_critic.set_annealing(
+                epochs=[0, self._cfgs.train_cfgs.epochs],
+                std=self._cfgs.model_cfgs.std_range,
+            )
+
+        self._init()
+
+        # Redo part of _init_log
+        what_to_save: dict[str, Any] = {}
+        what_to_save['pi'] = self._actor_critic.actor
+        what_to_save['actor_critic'] = self._actor_critic
+        if self._cfgs.algo_cfgs.obs_normalize:
+            obs_normalizer = self._env.save()['obs_normalizer']
+            what_to_save['obs_normalizer'] = obs_normalizer
+        if self._cfgs.algo_cfgs.reward_normalize:
+            reward_normalizer = self._env.save()['reward_normalizer']
+            what_to_save['reward_normalizer'] = reward_normalizer
+        if self._cfgs.algo_cfgs.cost_normalize:
+            cost_normalizer = self._env.save()['cost_normalizer']
+            what_to_save['cost_normalizer'] = cost_normalizer
+        self._logger.setup_torch_saver(what_to_save)
+        self._logger.torch_save()
+
+    def load(self, 
+        epoch: int, 
+        path: str,
+    ) -> None:
+        """Load a saved model.
+
+        Args:
+            epoch (int): The epoch to be loaded.
+            path (str): The path to the saved model that should be loaded.
+        """
+        self.__load_model_and_env(epoch=epoch, path=path)
+
+        self._start_epoch = epoch
+        self._logger.set_current_epoch(epoch)
+
+        self._logger.copy_from_csv(os.path.join(path, "progress.csv"))
+
+        csv_df = pd.read_csv(os.path.join(path, "progress.csv"))
+        final_epoch_df = csv_df[csv_df["Train/Epoch"] == epoch - 1]
+        self._cfgs.lagrange_cfgs["lagrangian_multiplier_init"] = final_epoch_df.iloc[0]["Metrics/LagrangeMultiplier"]
+        self._lagrange = Lagrange(**self._cfgs.lagrange_cfgs)
